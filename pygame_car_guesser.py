f2a7bc51a718ead786cd0c43ced0c1e16a3fde46
Andromeda Kepecs <amkepecs@gmail.com>
2023-06-17 20:04:36 -0700

    working image displayplay, todo fix cropped image and add guessing

--- pygame_car_guesser.py
+++ pygame_car_guesser.py
@@ -1,13 +1,66 @@
 import pygame, os, random
 
 CAPTION = "Car Guesser"
+CROP_WIDTH = 160
 WHITE = (255, 255, 255)
 FPS = 40
 
-def window():
+def get_input():
+	"""
+	Gets user input in pygame window
+	"""
+	pass
+
+def get_random_image_file():
+	"""
+	Gets random image from car-pictures directory
+	Return String file name of random image
+	"""
+	car_dir = os.getcwd() + "/car-pictures"
+
+	# Get random car file
+	car_f = random.choice([x for x in os.listdir(car_dir) if os.path.isfile(os.path.join(car_dir, x))])
+	return car_dir + "/" + car_f
+
+def crop_image(img, orig_x, orig_y):
+	"""
+	Randomly crop the image
+	Param orig_x, x coord of original image
+	Param orig_y, y coord of original image
+	Return cropped image
+	"""
+	# left = random.randint(0, img.get_width() - CROP_WIDTH)
+	# top = img.get_height() / 4
+	# right = left + CROP_WIDTH
+	# bottom = 3 * img.get_height() / 4
+	# cropped_img = img.crop((left, top, right, bottom))
+	# return pygame.image.load(cropped_img).convert()
+
+	x = random.randint(orig_x, orig_x + img.get_width() - CROP_WIDTH)
+	y = (orig_y + img.get_height()) / 4
+	width = x + CROP_WIDTH
+	height = 3 * y
+	return img.subsurface((x, y, width, height))
+
+
+def display_image(img, game_display, img_x, img_y):
+	"""
+	Displays image
+	Parameter img image
+	"""
+	game_display.blit(img, (img_x, img_y))
+	pygame.display.flip()
+
+def run_game():
+	"""
+	Runs the game
+	"""
+	# Open window and get display info
 	pygame.init()
 	display_info = pygame.display.Info()
-	game_display = pygame.display.set_mode((display_info.current_w, display_info.current_h))
+	width = display_info.current_w
+	height = display_info.current_h
+	game_display = pygame.display.set_mode((width, height))
 	pygame.display.set_caption(CAPTION)
 
 	clock = pygame.time.Clock()
@@ -19,13 +72,26 @@ def window():
 				quit = True
 
 		game_display.fill(WHITE)
+
+		# Display image
+		img_path = get_random_image_file()
+		img = pygame.image.load(img_path).convert()
+		img_x = width / 2 - img.get_width() / 2
+		img_y = height / 2 - img.get_height()
+
+		cropped_img = crop_image(img, img_x, img_y)
+		display_image(img, game_display, img_x, img_y)
+
 		pygame.display.update()
 		clock.tick(FPS)
 
 	pygame.quit()
 
 def main():
-	window()
+	"""
+	Main method
+	"""
+	run_game()
 
 if __name__ == "__main__":
 	main()
